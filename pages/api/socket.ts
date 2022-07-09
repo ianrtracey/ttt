cd8a0e45efb9c519fb6dbf5e806bc46ee96ef1aa
Ian Tracey <ianrtracey@gmail.com>
2022-07-09 02:09:21 -0400

    web sockets workin

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,3 +1,22 @@
-export default function handler (req: Request, res: Response) {
-  res.send('Hello World!')
+import { Server } from 'Socket.IO'
+
+const SocketHandler = (req: Request, res: Response) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', (socket: any) => {
+      console.log('connected to client')
+      socket.on('input-change', msg => {
+        console.log('msg', msg)
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+  }
+  res.end()
 }
+
+export default SocketHandler
